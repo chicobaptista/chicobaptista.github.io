e7c8a7979cd600063f533c0dcde89fb5ce0e26b8
Francisco Baptista <chico.baptista@gmail.com>
2018-04-16 21:42:05 -0300

    added debugging logs
    added some console.log() entries to try and debug why lang.js is not
    displaying on html correctly

--- lang.js
+++ lang.js
@@ -5,15 +5,18 @@ var langJS = null;
 
 var translate = function (jsdata)
 {	
+	console.log('Function called')
 	$("[tkey]").each (function (index)
 	{
 		var strTr = jsdata [$(this).attr ('tkey')];
 	    $(this).html (strTr);
+	    console.log('strTr is: '+strTr)
 	});
 }
 
 
 langCode = navigator.language.substr (0, 2);
+console.log('langCode is: '+langCode)
 
 if (langCode in langs)
 	$.getJSON('lang/'+langCode+'.json', translate);
